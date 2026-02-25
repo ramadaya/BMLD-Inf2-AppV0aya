71b1f324792fb5a61f8643c8f836cccbdf245b5a
Aya Ramadan <ramadaya@students.zhaw.ch>
2026-02-25 08:36:01 +0100

    emails sind hinzugefügt

--- views/home.py
+++ views/home.py
@@ -6,8 +6,9 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Aya Ramadan (ramadaya@students.zhaw.ch)
+- Carolina Tresch (tresccar@students.zhaw.ch)
+- Sofia Lercara (lercasof@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
