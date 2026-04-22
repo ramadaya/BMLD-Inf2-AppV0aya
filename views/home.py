0315d119c4cb1f97021d3dbb82b4a95f53f2e1dc
Aya Ramadan <ramadaya@students.zhaw.ch>
2026-04-22 08:40:15 +0200

    mit code gestartet

--- views/home.py
+++ views/home.py
@@ -1,6 +1,38 @@
 import streamlit as st
 
-st.title("Meine erste Streamlit App")
+st.title("Berry Cycle 🍓")
+
+# Begrüssung
+if "user" in st.session_state:
+    st.write(f"Hallo {st.session_state['user']} 👋")
+
+# Zyklustag
+zyklustag = st.number_input("Welcher Zyklustag bist du?", 1, 28)
+
+# Phase bestimmen
+if zyklustag <= 5:
+    phase = "Menstruation"
+    info = "Dein Körper braucht Ruhe 🧘‍♀️"
+elif zyklustag <= 13:
+    phase = "Follikelphase"
+    info = "Du hast mehr Energie 💪"
+elif zyklustag == 14:
+    phase = "Ovulation"
+    info = "Du bist besonders aktiv ⚡"
+else:
+    phase = "Lutealphase"
+    info = "Nimm dir Zeit für dich 💆‍♀️"
+
+# Anzeigen
+st.success(f"Aktuelle Phase: {phase}")
+st.info(info)
+
+
+
+
+
+
+
 
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
