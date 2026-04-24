29743be3fa6b92105612dbf489a6c77e5902bae0
CarolinaTresch <tresccar@students.zhaw.ch>
2026-04-24 11:06:06 +0200

    Synchronisation debug

--- views/3_Sport.py
+++ views/3_Sport.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import streamlit as st
 from cycle_utils import get_current_phase, PHASE_INFO
 
