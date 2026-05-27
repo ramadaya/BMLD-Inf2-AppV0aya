a49c0db1e2304369ad024054ccb810a4224d6030
Aya Ramadan <ramadaya@students.zhaw.ch>
2026-05-27 08:11:20 +0200

    code updated

--- app.py
+++ app.py
@@ -4,20 +4,13 @@ from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
-# --- NEW CODE: import and initialize data manager and login manager ---
-
-
-data_manager = DataManager(       # initialize data manager
-    fs_protocol='webdav',         # protocol for the filesystem, use webdav for switch drive
-    fs_root_folder="Berry_Cycle"  # folder on switch drive where the data is stored
+data_manager = DataManager(       
+    fs_protocol='webdav',         
+    fs_root_folder="Berry_Cycle"  
     )
 st.session_state["data_manager"] = data_manager 
-login_manager = LoginManager(data_manager) # handles user login and registration
-login_manager.login_register()             # stops if not logged in
-# --- END OF NEW CODE ---
-
-# --- CODE UPDATE: load user data from data manager if not already present in session state --
-
+login_manager = LoginManager(data_manager) 
+login_manager.login_register()             
 
 current_user = st.session_state.get("username")
 
@@ -27,14 +20,12 @@ if st.session_state.get("loaded_user") != current_user:
         initial_value=pd.DataFrame()
     )
     st.session_state["loaded_user"] = current_user
-# --- END OF CODE UPDATE ---
+
 pg_home = st.Page("views/1_home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/2_Kalender.py", title="📆 Kalender")
 pg_third = st.Page("views/3_Sport.py", title="👟 Sport")
 pg_fourth = st.Page("views/4_Ernährung.py", title="🍓 Ernährung")
 pg_fifth = st.Page("views/5_Symptome.py", title="🩺 Symptome")
 pg_sixth = st.Page("views/6_Boyfriend.py", title="❤️ Boyfriend")
-
-
 pg = st.navigation([pg_home, pg_second, pg_third, pg_fourth, pg_fifth, pg_sixth])
 pg.run()
